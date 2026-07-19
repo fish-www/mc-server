6a00da423219e8b0923f692d2a1f9c09ce188707
fishAwww <941456589@qq.com>
2026-07-19 16:37:27 +0800

    upgrade neoforge

--- server/run.sh
+++ server/run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-# Forge requires a configured set of both JVM and program arguments.
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
@@ -15,4 +14,5 @@ if [ ! -x "$JAVA_BIN" ]; then
     exit 1
 fi
 
-exec taskset -c 0-7,16-23 "$JAVA_BIN" @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.1.228/unix_args.txt nogui "$@"
+exec taskset -c 0-7,16-23 "$JAVA_BIN" @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.1.234/unix_args.txt nogui "$@"
+
